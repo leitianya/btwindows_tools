2dc2b335de11e7eb521af5420f7ad3f1b31a987e
gacjie <90205428+gacjie@users.noreply.github.com>
2022-02-11 14:12:28 +0800

    Update btwindows_tools.bat

--- btwindows_tools.bat
+++ btwindows_tools.bat
@@ -1,2 +1,2 @@
-wget https://gitee.com/gacjie/btwindows_tools/raw/master/btwindows_tools.py -O C:/btwindows_tools.py -t 5 -T 60 
+wget https://raw.githubusercontent.com/gacjie/btwindows_tools/main/btwindows_tools.py -O C:/btwindows_tools.py -t 5 -T 60 
 python C:/btwindows_tools.py
